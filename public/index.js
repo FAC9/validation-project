8cfe5715a4405ac904ed2d763e899ab67c043824
Cleop <cleo.pearson@hotmail.co.uk>
2016-11-18 13:11:25 +0000

    Fix bug to update DOM

--- public/index.js
+++ public/index.js
@@ -1,23 +1,24 @@
 // endpoint is /signup
 console.log("into index.js")
-var username = document.getElementById('username').value;
-var password = document.getElementById('password').value;
 var submit_button = document.getElementById('btn_submit');
-var signup_message = document.getElementById('signup_message').textContent;
+var signup_message = document.getElementById('signup_message');
 
 submit_button.addEventListener('click', function() {
+  var userid = document.getElementById('username').value;
+  var password = document.getElementById('password').value;
   console.log('click');
-  var url = '/signup?username=' + username + '&password=' + password;
-  // signup_message = "";
+  var url = '/signup?userid=' + userid + '&password=' + password;
+  signup_message.textContent = "";
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status == 200) {
       console.log("received successful response");
-        signup_message = 'Account created, check your emails to verify your account.'
+        signup_message.textContent = 'Account created, check your emails to verify your account.'
     }
     else {
       console.log("received error ");
-       signup_message = 'Error: Username must be an email address, password must contain 6-10 characters, at least one capital letter, one digit and no symbols.'
+      console.log(xhr.response);
+       signup_message.textContent = 'Error: Username must be an email address, password must be at least 6 characters, one lowercase, one uppercase and one digit'
     }
   };
   xhr.open('GET', url, true);
