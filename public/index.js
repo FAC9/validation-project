6115625e2c06b6aa7265d7f9037e0781b5d2aa8c
marisid <sid_marina@hotmail.com>
2016-11-18 12:55:26 +0000

    Add console.log statements for debugging

--- public/index.js
+++ public/index.js
@@ -1,19 +1,22 @@
 // endpoint is /signup
-
+console.log("into index.js")
 var username = document.getElementById('username').value;
 var password = document.getElementById('password').value;
-var submit_button = document.getElementById('btn_submit').value;
+var submit_button = document.getElementById('btn_submit');
 var signup_message = document.getElementById('signup_message').textContent;
 
 submit_button.addEventListener('click', function() {
+  console.log('click');
   var url = '/signup?username=' + username + '&password=' + password;
-  signup_message = "";
+  // signup_message = "";
   var xhr = new XMLHttpRequest();
   xhr.onreadystatechange = function() {
     if (xhr.readyState === 4 && xhr.status == 200) {
+      console.log("received successful response");
         signup_message = 'Account created, check your emails to verify your account.'
     }
     else {
+      console.log("received error ");
        signup_message = 'Error: Username must be an email address, password must contain 6-10 characters, at least one capital letter, one digit and no symbols.'
     }
   };
